57969f9d52f1c22eb7df677e3f8692e43ce73e55
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-07-25 04:06:18 +0530

    Create 493. Reverse Pairs LC ⭐⭐⭐.cpp

---
+++ Day - 3/493. Reverse Pairs LC ⭐⭐⭐.cpp
@@ -0,0 +1,31 @@
+/*
+This is a great use of divide and conquer paradigm
+Recursively sort the array and before merging two sub-arrays
+check for any given condition and find the count
+
+*/
+
+
+
+#define itr vector<int>::iterator 
+class Solution {
+public:
+    int CountPair(itr begin, itr end){
+        if(end-begin <= 1) return 0;
+        itr mid = begin + (end-begin)/2;
+        int count = 0;
+        if(mid>begin) count += CountPair(begin,mid);
+        if(mid<end) count += CountPair(mid,end);
+        
+        for(itr i=begin,j=mid;i<mid;i++){
+            while(j<end && (*i) > 2ll*(*j)) j++;
+            count += (j-mid);
+        }
+        inplace_merge(begin, mid, end);
+        return count;   
+    }
+    
+    int reversePairs(vector<int>& nums) {
+        return CountPair(nums.begin(),nums.end());
+    }
+};
